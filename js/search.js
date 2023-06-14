d7b20df763b73704887ab86b1f74a0fd27e06703
정우성 <jws031024@gmail.com>
2023-06-14 11:24:34 +0000

    실습 7주차

--- js/search.js
+++ js/search.js
@@ -1,13 +1,17 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
+var search_array = []; // 빈 배열 – 전역 변수
+
 function search_message(){
-      let search_str = document.querySelector("#search_txt"); // 변수에 저장
-		if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+   let search_str = document.querySelector("#search_txt");
+    if(search_str.value.length === 0){
        alert("검색어가 비었습니다. 입력해주세요"); 
     }
-	else{
+    else{
        alert("검색을 수행합니다!");
-       let text = document.getElementById("search_message").innerHTML = search_str.value;
-       document.querySelector("#form_main").submit();
+       search_array.push(search_str.value); // 배열에 검색어 추가
+       let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
+        document.querySelector("#form_main").submit();
     }
 }
+
